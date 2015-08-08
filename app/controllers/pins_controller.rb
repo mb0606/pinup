57a4e05e3598727f3948104c0a13cf9a77afc6c3
Marco Berardini <mberardini@evolution-man.com>
2015-08-08 14:51:16 -0700

    Add and configure Gem acts as votable

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,7 @@
 class PinsController < ApplicationController
 
-	before_action :find_pin, only: [:show, :edit, :update, :destroy]
+	before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote]
+	before_action :authenticate_user! , except: [:index, :show]
 
 	def index
 		@pins = Pin.all.order("created_at DESC")
@@ -42,6 +43,11 @@ class PinsController < ApplicationController
 		@pin.destroy
 		redirect_to root_path
 	end
+
+	def upvote
+		@pin.upvote_by current_user
+		redirect_to :back
+	end 
 
 	
 
